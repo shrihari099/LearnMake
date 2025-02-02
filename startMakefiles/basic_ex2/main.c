4f60a15e50a349b01e8f81fc476ee8dfd100c184
shriharivaidya099 <shriharivaidya099@gmail.com>
2025-02-02 23:34:26 +0530

    2nd refererence code

---
+++ startMakefiles/basic_ex2/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<header.h>
+void main(){
+    printf("iam in main \n");
+    int a=10,b=20,c=0;
+    c= add(a,b);
+    printSum(c);
+}
---
+++ startMakefiles/basic_ex2/printSum.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+void printSum(int c){
+    printf("%d sum is =  \n",c);
+}
